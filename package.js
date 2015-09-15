412ce64b463df2d80098fc9742dab9082d77c97a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2015-09-15 18:07:08 +0200

    Added github url

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'msx-compiler',
+  name: 'grottolabs:msx-compiler',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'MSX compiler for Mithril',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Grottolabs/meteor-msx-compiler',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -27,6 +27,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('msx-compiler');
+  api.use('grottolabs:msx-compiler');
   api.addFiles('msx-compiler-tests.js');
 });
